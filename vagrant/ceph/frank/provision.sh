a83edb976beeddbbfd0767cb406b41674ed48da6
Noah Mehl <noah@tritonlimited.com>
2013-04-10 19:58:00 -0400

    Changed ceph vagrant guests to Ubuntu Precise, added root ssh keys, added daisey client cert

--- vagrant/ceph/frank/provision.sh
+++ vagrant/ceph/frank/provision.sh
@@ -11,4 +11,11 @@ echo "http_proxy=\"http://training-puppet-master:3128/\"" >> /etc/environment
 echo "ftp_proxy=\"http://training-puppet-master:3128/\"" >> /etc/environment
 echo "https_proxy=\"http://training-puppet-master:3128/\"" >> /etc/environment
 
-cp -a /vagrant/puppet /etc/
+sudo cp -a /vagrant/.ssh /root/
+sudo chown -R root:root /root/.ssh
+sudo chmod 700 /root/.ssh
+sudo chmod 600 /root/.ssh/id_rsa
+sudo chmod 644 /root/.ssh/id_rsa.pub
+sudo chmod 600 /root/.ssh/authorized_keys2
+sudo cp -a /vagrant/puppet /etc/
+sudo chown -R root:root /etc/puppet
