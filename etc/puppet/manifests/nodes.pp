40b7e22af2f4b5a005ccb1eced2a7e3bd5a11fe1
Noah Mehl <noah.mehl@hastexo.com>
2013-05-04 17:27:37 +0000

    Imported openstack module and began with openstack on bob

--- etc/puppet/manifests/nodes.pp
+++ etc/puppet/manifests/nodes.pp
@@ -9,6 +9,29 @@ node bob {
   class { "ceph": }
   class { "ceph-packages": }
   class { "ceph-openstack": }
+  class { 'openstack::controller':
+  require                 => Class['ceph'],
+  public_address          => '192.168.122.112',
+  public_interface        => 'eth1',
+  private_interface       => 'eth2',
+  internal_address        => '192.168.133.112',
+  floating_range          => '192.168.101.64/28',
+  fixed_range             => '10.0.0.0/24',
+  multi_host              => false,
+  network_manager         => 'nova.network.manager.FlatDHCPManager',
+  admin_email             => 'admin@test.com',
+  admin_password          => 'admin_password',
+  secret_key              => 'secret_key',
+  mysql_root_password     => 'mysql_root_password',
+  keystone_admin_token    => 'keystone_admin_token',
+  keystone_db_password    => 'keystone_db_password',
+  glance_user_password    => 'glance_user_password',
+  glance_db_password      => 'glance_db_password',
+  nova_user_password      => 'nova_user_password',
+  nova_db_password        => 'nova_db_password',
+  rabbit_password         => 'rabbit_password',
+  rabbit_user             => 'rabbit_user',
+  }
 }
 
 node charlie {
